d194a19b2610a17d5a85812069846fd222243e2f
barukh <bshw770@gmail.com>
2025-07-15 21:25:55 +0300

    file ops done for now

--- p-14/file_ops.c
+++ p-14/file_ops.c
@@ -16,11 +16,18 @@ FILE* open_file(const char* file_name, char* file_ext, char mode) {
 }
 
 char* read_line(FILE* file, int max_read, char* buffer) {
-    
+    char* line = fgets(buffer, max_read, file);
+    if (NULL == line)  {
+        return NULL;
+    }
+    if (line[max_read-1] != '\n') {
+        /*error*/
+    }
+    return line;
 }
 
 int write_line(FILE* file, char* buffer) {
-
+    return fputs(buffer, file);
 }
 
 int close_file(FILE* file) {
---
+++ p-14/macro_unfold.h
@@ -0,0 +1,5 @@
+#ifndef MACRO_UNFOLD_H
+#define MACRO_UNFOLD_H 1
+
+
+#endif
